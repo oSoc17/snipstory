368e9ff07824b0c068515afe060589843fde8d4e
Elias Meire <me@eliasmei.re>
2017-07-10 15:23:50 +0200

    move app files, connect to store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { test } from '../redux/actions';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.test();
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>iShare</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  user: state.user,
+  room: state.room
+});
+
+export default connect(mapStateToProps, { test })(App);

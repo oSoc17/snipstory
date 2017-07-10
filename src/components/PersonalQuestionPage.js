707ff25ff1002b761870c631fa36f497e21dda82
Brysen Ackx <brysen.ackx@gmail.com>
2017-07-10 21:11:50 +0200

    Question page blueprint added

---
+++ src/components/PersonalQuestionPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const PersonalQuestionPage = ({question, choices}) => (
+  <div>
+    <span>{question}</span>
+    <div id={choices[0]}>{choices[0]}</div>
+    <div id={choices[1]}>{choices[1]}</div>
+    <div id={choices[2]}>{choices[2]}</div>
+    <div id={choices[3]}>{choices[3]}</div>
+  </div>
+);

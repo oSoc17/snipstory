4dde880206f2f5fac4b7c95982552ad9d4c119ee
Elias Meire <me@eliasmei.re>
2017-07-12 14:47:48 +0200

    initial state of room

--- src/redux/modules/room.js
+++ src/redux/modules/room.js
@@ -1,6 +1,13 @@
 import { actionTypes } from '../actions';
 
-const initialState = {};
+const initialState = {
+  id: '',
+  name: '',
+  period: '',
+  modules: null,
+  tags: null,
+  isLoading: false
+};
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {

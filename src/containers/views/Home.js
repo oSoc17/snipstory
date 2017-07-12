a7b170fa6a9efba5638b3e543c35c2316ac9f71a
Brysen Ackx <brysen.ackx@gmail.com>
2017-07-12 14:12:07 +0200

    started adding story selection

--- src/containers/views/Home.js
+++ src/containers/views/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { firebaseDatabase } from '../../helpers/firebase';
 import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
+  loadRandomStories() {
+    firebaseDatabase.ref('/stories').once('value').then(snapshot => {
+      snapshot.val();
+    });
+  }
+
   render() {
     return (
-      <div className="page">
-        <button value="teacher">Teacher</button>
-        <Link to="/quiz">Start</Link>
-        <div className="filters" />
+      <div>
+        <div className="page">
+          <button value="teacher">Teacher</button>
+          <Link to="/quiz">Start</Link>
+          <div className="filters" />
+        </div>
+        <div className="page">loadRandomStories().map(story => )</div>
       </div>
     );
   }

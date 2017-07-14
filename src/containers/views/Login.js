d6fef07e0306b8291481cd238026e132570eb231
Elias Meire <me@eliasmei.re>
2017-07-14 19:57:50 +0200

    Login, register, teacherarea page

--- src/containers/views/Login.js
+++ src/containers/views/Login.js
@@ -1,6 +1,68 @@
 import React from 'react';
+import Button from '../../components/button/Button';
+import { firebaseAuth, googleAuthProvider } from '../../helpers/firebase';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm, SubmissionError } from 'redux-form';
+import { LogIn } from 'react-feather';
+import GoogleLogo from './google.svg';
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ pristine, submitting, handleSubmit, error }) => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>
+        <Button
+          onClick={() => {
+            firebaseAuth.signInWithPopup(googleAuthProvider).catch(err => {
+              throw new SubmissionError({
+                _error: 'Login with Google failed'
+              });
+            });
+          }}
+        >
+          <img src={GoogleLogo} alt="Google logo" />
+          <span>Login met Google</span>
+        </Button>
+        <form
+          onSubmit={handleSubmit(({ email, password }) => {
+            return firebaseAuth
+              .signInWithEmailAndPassword(email, password)
+              .catch(_ => {
+                throw new SubmissionError({
+                  _error:
+                    'Sorry, you entered an incorrect email address or password.'
+                });
+              });
+          })}
+        >
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field
+              name="email"
+              component="input"
+              type="email"
+              placeholder="jane@example.com"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Paswoord</label>
+            <Field name="password" component="input" type="password" />
+          </div>
+          {error &&
+            <div>
+              {error}
+            </div>}
+          <Button type="submit" disabled={pristine || submitting}>
+            <LogIn />
+            <span>Login</span>
+          </Button>
+        </form>
+      </div>
+      <div>
+        <span>Heb je nog geen account?</span>
+        <Link to="/teacher/register">Registreer hier!</Link>
+      </div>
+    </div>
+  );
 };
-export default Login;
+export default reduxForm({ form: 'login' })(Login);
--- src/containers/views/Register.js
+++ src/containers/views/Register.js
@@ -1,6 +1,84 @@
 import React from 'react';
-
-const Register = () => {
-  return <div>Register</div>;
+import Button from '../../components/button/Button';
+import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+import {
+  firebaseAuth,
+  googleAuthProvider,
+  firebaseDatabase
+} from '../../helpers/firebase';
+import GoogleLogo from './google.svg';
+const Register = ({ pristine, submitting, handleSubmit, history }) => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <div>
+        <Button
+          onClick={() => {
+            firebaseAuth.signInWithRedirect(googleAuthProvider);
+            history.push('/teacher');
+          }}
+        >
+          <img src={GoogleLogo} alt="Google logo" />
+          <span>Registreer met Google</span>
+        </Button>
+        <form
+          onSubmit={handleSubmit(({ name, email, password }) => {
+            firebaseAuth
+              .createUserWithEmailAndPassword(email, password)
+              .then(user => {
+                firebaseDatabase
+                  .ref(`/users/${user.uid}`)
+                  .set({ ...user.providerData[0], displayName: name })
+                  .then(_ => {
+                    history.push('/login');
+                  });
+              });
+          })}
+        >
+          <div>
+            <label htmlFor="name">Naam</label>
+            <Field
+              name="name"
+              component="input"
+              type="name"
+              placeholder="Jane Doe"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">E-mail</label>
+            <Field
+              name="email"
+              component="input"
+              type="email"
+              placeholder="jane@example.com"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Paswoord</label>
+            <Field name="password" component="input" type="password" />
+          </div>
+          <div>
+            <label htmlFor="password1">Herhaal paswoord</label>
+            <Field name="password1" component="input" type="password" />
+          </div>
+          <Button type="submit" disabled={pristine || submitting}>
+            Register
+          </Button>
+        </form>
+      </div>
+      <div>
+        <span>Heb je al een account?</span>
+        <Link to="/teacher/login">Log hier in!</Link>
+      </div>
+    </div>
+  );
 };
-export default Register;
+
+const validate = ({ password, password1, email }) => {
+  const errors = {};
+  if (password !== password1) {
+    errors.password1 = 'Paswoorden zijn niet gelijk';
+  }
+};
+export default reduxForm({ form: 'login', validate })(Register);

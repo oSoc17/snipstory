b8e8fcccd325b5d4d0ea9da6ba7c64d6db94bebc
Brysen Ackx <brysen.ackx@gmail.com>
2017-07-17 11:45:54 +0200

    fixed modules + added funfact module

--- src/containers/views/Room.js
+++ src/containers/views/Room.js
@@ -1,19 +1,21 @@
-import React from "react";
-import { firebaseAuth } from "../../helpers/firebase";
-import { connect } from "react-redux";
+import React from 'react';
+import moment from 'moment';
+import { firebaseAuth } from '../../helpers/firebase';
+import { connect } from 'react-redux';
 import {
   fetchRoomData,
   listenForRoomChange,
   updateModule
-} from "../../redux/actions";
-import ImageModule from "../../components/modules/ImageModule";
-import ImageQuizModule from "../../components/modules/ImageQuizModule";
-import MapModule from "../../components/modules/MapModule";
-import QuizModule from "../../components/modules/QuizModule";
-import SearchExerciseModule from "../../components/modules/SearchExerciseModule";
-import TextblockModule from "../../components/modules/TextblockModule";
-import VideoModule from "../../components/modules/VideoModule";
-import YoutubeModule from "../../components/modules/YoutubeModule";
+} from '../../redux/actions';
+import ImageModule from '../../components/modules/ImageModule';
+import ImageQuizModule from '../../components/modules/ImageQuizModule';
+import MapModule from '../../components/modules/MapModule';
+import QuizModule from '../../components/modules/QuizModule';
+import SearchExerciseModule from '../../components/modules/SearchExerciseModule';
+import TextblockModule from '../../components/modules/TextblockModule';
+import VideoModule from '../../components/modules/VideoModule';
+import YoutubeModule from '../../components/modules/YoutubeModule';
+import FunFactModule from '../../components/modules/FunFactModule';
 
 class Room extends React.Component {
   handleChange(module) {
@@ -43,12 +45,30 @@ class Room extends React.Component {
           <h1>
             {room.name}
           </h1>
+          <h2>
+            {moment(room.birthdate, 'DD-MM-YYYY').format('DD/MM/YYYY') +
+              ' - ' +
+              moment(room.died, 'DD-MM-YYYY').format('DD/MM/YYYY')}
+          </h2>
+          <h3>
+            {room.nationality}
+          </h3>
+          <div>
+            Leeftijd
+            <div>
+              {moment(room.died, 'DD-MM-YYYY').diff(
+                moment(room.birthdate, 'DD-MM-YYYY').format(''),
+                'years'
+              )}
+            </div>
+          </div>
         </div>
         <div className="modules">
           {room.modules &&
             room.modules.map((module, i) => {
+              console.log(module);
               switch (module.contentType.toLowerCase()) {
-                case "image":
+                case 'image':
                   return (
                     <ImageModule
                       index={i}
@@ -58,7 +78,7 @@ class Room extends React.Component {
                       user={user}
                     />
                   );
-                case "imagequiz":
+                case 'imagequiz':
                   return (
                     <ImageQuizModule
                       index={i}
@@ -69,7 +89,7 @@ class Room extends React.Component {
                       handleChange={this.handleChange.bind(this)}
                     />
                   );
-                case "map":
+                case 'map':
                   return (
                     <MapModule
                       index={i}
@@ -80,7 +100,7 @@ class Room extends React.Component {
                       handleChange={this.handleChange.bind(this)}
                     />
                   );
-                case "quiz":
+                case 'quiz':
                   return (
                     <QuizModule
                       index={i}
@@ -91,7 +111,7 @@ class Room extends React.Component {
                       handleChange={this.handleChange.bind(this)}
                     />
                   );
-                case "searchex":
+                case 'searchex':
                   return (
                     <SearchExerciseModule
                       index={i}
@@ -101,7 +121,7 @@ class Room extends React.Component {
                       user={user}
                     />
                   );
-                case "textblock":
+                case 'textblock':
                   return (
                     <TextblockModule
                       index={i}
@@ -111,7 +131,7 @@ class Room extends React.Component {
                       user={user}
                     />
                   );
-                case "video":
+                case 'video':
                   return (
                     <VideoModule
                       index={i}
@@ -121,7 +141,7 @@ class Room extends React.Component {
                       user={user}
                     />
                   );
-                case "youtube":
+                case 'youtube':
                   return (
                     <YoutubeModule
                       index={i}
@@ -131,8 +151,18 @@ class Room extends React.Component {
                       user={user}
                     />
                   );
+                case 'funfact':
+                  return (
+                    <FunFactModule
+                      index={i}
+                      key={i}
+                      module={module}
+                      users={room.users}
+                      user={user}
+                    />
+                  );
                 default:
-                  return <div />;
+                  return <div key={i} />;
               }
             })}
         </div>

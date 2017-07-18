d5634497957a560a2204a6897e343ca5245937a4
remcovb <remcovanbogaert@student.howest.be>
2017-07-18 15:04:57 +0200

    include reset.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
 import './index.css';
 import Root from './containers/Root';
 import registerServiceWorker from './registerServiceWorker';

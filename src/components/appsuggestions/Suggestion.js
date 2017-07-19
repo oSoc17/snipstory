3acc2423ff8068fc855946fdf4f2086a9fea0bf5
Brysen Ackx <brysen.ackx@gmail.com>
2017-07-19 21:50:27 +0200

    alignment

--- src/components/appsuggestions/Suggestion.js
+++ src/components/appsuggestions/Suggestion.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Suggestion = props =>
-  <div className="suggestion row col-md-6">
+  <div className="suggestion col-md-6">
     <img src={props.image} alt="" height="50px" />
     <span className="col-md-6">
       {props.name}
--- src/components/uploadbox/UploadBox.js
+++ src/components/uploadbox/UploadBox.js
@@ -21,39 +21,45 @@ class UploadBox extends React.Component {
 
   render() {
     return (
-      <div className="upload">
-        <span>Voeg een foto of video toe</span>
-        <div>
-          <input
-            type="file"
-            id="creationUpload"
-            name="creationUpload"
-            onChange={e => {
-              this.props.uploadFile(e.target.files[0]);
-            }}
-          />
-        </div>
-        <div>
+      <div className="upload container">
+        <div className="form-group">
+          <h2>Voeg een foto of video van jouw snipper toe</h2>
+          <div>
+            <input
+              type="file"
+              id="creationUpload"
+              name="creationUpload"
+              onChange={e => {
+                this.props.uploadFile(e.target.files[0]);
+              }}
+            />
+          </div>
           {this.props.creation.photoURL &&
             <img src={this.props.creation.photoURL} alt="upload" />}
         </div>
-        <label htmlFor="creators">Vul jullie naam in: </label>
-        <input
-          type="text"
-          name="creators"
-          id="creators"
-          onChange={this.props.addCreatorsToCreation}
-        />
-        <label htmlFor="creation-description">
-          Wil je iets over je snipper schrijven?{' '}
-        </label>
-        <textarea
-          name="creation-description"
-          onChange={this.props.addDescriptionToCreation}
-          id="creation-description"
-          cols="30"
-          rows="10"
-        />
+        <div className="form-group">
+          <label htmlFor="creators">Vul jullie naam in: </label>
+          <br />
+          <input
+            type="text"
+            name="creators"
+            id="creators"
+            onChange={this.props.addCreatorsToCreation}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="creation-description">
+            Wil je iets over je snipper schrijven?{' '}
+          </label>
+          <br />
+          <textarea
+            name="creation-description"
+            onChange={this.props.addDescriptionToCreation}
+            id="creation-description"
+            cols="30"
+            rows="10"
+          />
+        </div>
       </div>
     );
   }

308073aedfcd304679e18edb8cf91854ffbd5d4d
Brysen Ackx <brysen.ackx@gmail.com>
2017-07-20 11:35:07 +0200

    suggestions structure + basic styling

--- src/containers/views/KnutselTips.js
+++ src/containers/views/KnutselTips.js
@@ -14,20 +14,35 @@ class KnutselTips extends React.Component {
     return (
       <div>
         <NavBar />
-        <h1>Inspiratie</h1>
-        {knutseltips.tips.map((tip, i) =>
-          <div className="knutseltip" key={i}>
-            <div
-              style={{
-                backgroundImage: 'url(' + tip.image + ')',
-                height: '100px',
-                width: '100px',
-                backgroundSize: 'cover'
-              }}
-            />
-            {tip.name}
+        <div className="container text-center knutseltips">
+          <h1>Knutseltips</h1>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              flexWrap: 'wrap'
+            }}
+          >
+            {knutseltips.tips.map((tip, i) =>
+              <div key={i}>
+                <div className="knutseltip m-3">
+                  <div
+                    className="d-inline-block rounded-circle"
+                    style={{
+                      backgroundImage: 'url(' + tip.image + ')',
+                      height: '100px',
+                      width: '100px',
+                      backgroundSize: 'cover'
+                    }}
+                  />
+                  <span className="d-inline-block">
+                    {tip.name}
+                  </span>
+                </div>
+              </div>
+            )}
           </div>
-        )}
+        </div>
         <Footer />
       </div>
     );
--- src/redux/modules/knutseltips.js
+++ src/redux/modules/knutseltips.js
@@ -2,20 +2,25 @@ import { actionTypes } from '../actions';
 
 const initialState = {
   error: '',
-  tips: []
+  tips: [],
+  isFetching: false
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.fetchKnutselTipsStarted:
-      return state;
+      return Object.assign({}, state, {
+        isFetching: true
+      });
     case actionTypes.fetchKnutselTipsFulfilled:
       return Object.assign({}, state, {
-        tips: action.knutselTips
+        tips: action.knutselTips,
+        isFetching: false
       });
     case actionTypes.fetchKnutselTipsRejected:
       return Object.assign({}, state, {
-        error: action.error
+        error: action.error,
+        isFetching: false
       });
     default:
       return state;

bf5248bea90f0dbd0e013d7d27938ac0a55958e8
Elias Meire <me@eliasmei.re>
2017-07-20 11:27:22 +0200

    Home changes

--- src/components/creations1/Creations1.js
+++ src/components/creations1/Creations1.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import Button from '../button/Button';
-import TestPhoto from '../../containers/views/Home/assets/test-photo.jpg';
 import './Creations1.css';
+import { history } from '../../redux/store';
 
-const Creations1 = () => {
+const Creations1 = ({ snipper }) => {
   return (
     <article className="article-container-1">
-      <div className="creations-container-1">
-        <img src={TestPhoto} alt="" className="img-mapping-1" />
+      <div
+        className="creations-container-1"
+        onClick={e => history.push(`/snippers/${snipper.id}`)}
+      >
+        <img
+          src={snipper.photoURL}
+          alt={`Snipper van ${snipper.creators}`}
+          className="img-mapping-1"
+        />
         <div>
           <h2 className="article-title">
-            Door Eli Lambert, Charlotte Roels, Olivia Severs & Noora de Grote
+            Door {snipper.creators}
           </h2>
           <p className="article-para">
-            We hebben een filmpje gemaakt rond Thomas Reddy
+            {snipper.description}
           </p>
         </div>
       </div>
-      <Button inverted to="/">
+      <Button inverted to={`/story/select?id=${snipper.storyId}`}>
         Ontdek dit verhaal zelf
       </Button>
     </article>

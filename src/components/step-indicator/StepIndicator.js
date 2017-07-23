4a76f361cdc80397101eb309361399c0f005596f
Elias Meire <me@eliasmei.re>
2017-07-23 23:48:48 +0200

    step indicator styling

--- src/components/step-indicator/StepIndicator.js
+++ src/components/step-indicator/StepIndicator.js
@@ -5,10 +5,12 @@ const StepIndicator = ({ step, title, description, image }) => {
     <div className="step-indicator">
       <div className="container">
         <div className="row">
-          <div className="step-number display-1 col-md-3">
-            {step}
+          <div className="display-1 col-md-3">
+            <div className="step-number">
+              {step}
+            </div>
           </div>
-          <div className="step-indicator__body col-md-6">
+          <div className="step-indicator__body col-md-4">
             <h1 className="step-indicator__title">
               {title}
             </h1>
@@ -17,7 +19,7 @@ const StepIndicator = ({ step, title, description, image }) => {
             </p>
           </div>
           <img
-            className="step-indicator__image col-md-3"
+            className="step-indicator__image col-md-5"
             src={image}
             alt={title}
           />

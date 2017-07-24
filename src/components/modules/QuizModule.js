4e724c6dc89097c0ef460e67df1ccffb8c2d620b
Brysen Ackx <brysen.ackx@gmail.com>
2017-07-24 09:50:15 +0200

    module feedback working

--- src/components/modules/QuizModule.js
+++ src/components/modules/QuizModule.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Button from '../button/Button';
+import React from "react";
+import Button from "../button/Button";
 
 const selectAnswer = (module, index, handleChange) => {
   handleChange(
@@ -18,25 +18,60 @@ const QuizModule = ({ module, handleChange, users }) => {
       </p>
 
       <div className="question">
-        <div className="question-text">
-          <span className="questionmark">?</span>
-          <span>
-            {module.question}
-          </span>
+        {module.correction === module.answer
+          ? <div>
+              {module.correctMessage}
+            </div>
+          : <div className="question-text">
+              <span className="questionmark">?</span>
+              <span>
+                {module.question}
+              </span>
+            </div>}
+        <div>
+          {users[module.clickedBy]}
         </div>
 
-        <div>{users[module.clickedBy]}</div>
-
         <div className="buttons">
-          {module.options.map((option, i) =>
-            <Button
-              inverted
-              key={i}
-              onClick={_ => selectAnswer(module, i, handleChange)}
-            >
-              {option}
-            </Button>
-          )}
+          {module.options.map((option, i) => {
+            if (
+              module.answer === module.correction &&
+              module.correction === i &&
+              module.answer === i
+            ) {
+              return (
+                <Button
+                  inverted
+                  key={i}
+                  onClick={_ => selectAnswer(module, i, handleChange)}
+                  style={{ backgroundColor: "green" }}
+                >
+                  {option}
+                </Button>
+              );
+            } else if (module.correction !== i && module.answer === i) {
+              return (
+                <Button
+                  inverted
+                  key={i}
+                  onClick={_ => selectAnswer(module, i, handleChange)}
+                  style={{ backgroundColor: "red" }}
+                >
+                  {option}
+                </Button>
+              );
+            } else {
+              return (
+                <Button
+                  inverted
+                  key={i}
+                  onClick={_ => selectAnswer(module, i, handleChange)}
+                >
+                  {option}
+                </Button>
+              );
+            }
+          })}
         </div>
       </div>
     </article>

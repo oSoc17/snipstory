a3acf9051ba6090df13697df6f5d1483d2848355
Elias Meire <me@eliasmei.re>
2017-07-25 12:23:18 +0200

    video thumbnail

--- src/containers/views/Home/Home.js
+++ src/containers/views/Home/Home.js
@@ -10,16 +10,30 @@ import logo from './assets/logo.png';
 // import ht2 from './assets/ht-2.svg';
 // import ht3 from './assets/ht-3.svg';
 // import ht4 from './assets/ht-4.svg';
+
+import thumbnail from './assets/thumb.png';
 import Spinner from '../../../components/spinner/Spinner';
 import { fetchRandomSnippers } from '../../../redux/actions';
 import { connect } from 'react-redux';
+import { Play } from 'react-feather';
 
 import './Home.css';
 
 class Home extends React.Component {
+  state = { videoShown: false, thumbFadeout: false };
   componentWillMount() {
     this.props.fetchRandomSnippers();
   }
+
+  playVideo(event) {
+    this.setState({ thumbFadeout: true });
+    setTimeout(() => {
+      this.setState(prevState => {
+        return { ...prevState, videoShown: true };
+      });
+    }, 100);
+  }
+
   render() {
     const { randomSnippers, isLoading } = this.props;
     return (
@@ -76,13 +90,33 @@ class Home extends React.Component {
           <section className="video-section">
             <div className="container">
               <iframe
+                id="vimeo-video"
                 title="Wat is Snipstory?"
-                src="https://player.vimeo.com/video/226714616?color=3778FB&title=0&byline=0&portrait=0"
-                width="640"
-                height="360"
+                src={`https://player.vimeo.com/video/226714616?color=3778FB&title=0&byline=0&portrait=0&autoplay=${this
+                  .state.videoShown}`}
+                width="1920"
+                height="1080"
                 frameBorder="0"
                 allowFullScreen
               />
+              <div
+                className={`video-placeholder${this.state.thumbFadeout
+                  ? ' fade-out'
+                  : ''}`}
+                style={{
+                  backgroundImage: `url(${thumbnail})`,
+                  zIndex: this.state.videoShown ? -1 : 2
+                }}
+              >
+                <button
+                  onClick={this.playVideo.bind(this)}
+                  className="video-play"
+                >
+                  <div className="video-play__icon">
+                    <Play style={{ paddingLeft: '.1rem' }} size={128} />
+                  </div>
+                </button>
+              </div>
             </div>
           </section>
           {!randomSnippers || isLoading

544a6724beb639772850d74960175a5717315c9f
Brysen Ackx <brysen.ackx@gmail.com>
2017-07-25 12:10:23 +0200

    knutseltips squares

--- src/components/difficulty/Difficulty.js
+++ src/components/difficulty/Difficulty.js
@@ -3,15 +3,12 @@ import Bol from './bol';
 
 const Difficulty = props => {
   let data = [];
-  console.log(props.amount);
   for (let i = 1; i <= props.amount; i++) {
     data.push(<Bol filled={true} key={i} />);
   }
   for (let j = 1; j <= 4 - props.amount; j++) {
     data.push(<Bol filled={false} key={props.amount + j} />);
   }
-
-  console.log(data);
 
   return (
     <div className="difficulty">

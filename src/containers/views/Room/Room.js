9a53b963f881485c0b142cd7140a0ab8d620b7c7
remcovb <remcovanbogaert@student.howest.be>
2017-07-26 12:59:37 +0200

    resolve problems

--- src/containers/views/Room/Room.js
+++ src/containers/views/Room/Room.js
@@ -124,117 +124,6 @@ class Room extends React.Component {
               className="card-img-top-2"
               src={room.profilePicture}
               alt={room.name}
-<<<<<<< HEAD
-            />
-          </div>
-
-          <div
-            className="users"
-            style={{
-              position: 'absolute',
-              right: '0',
-              top: '0',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center'
-            }}
-          >
-            {Object.keys(room.users).length > 1 &&
-              <div>
-                <h4>Mensen in dit verhaal</h4>
-                {Object.keys(room.users).map(key => {
-                  return (
-                    <div key={key} style={{ verticalAlign: 'center' }}>
-                      <User />
-                      {room.users[key]}
-                    </div>
-                  );
-                })}
-              </div>}
-            <h4>Nodig iemand uit om mee te werken:</h4>
-            <input
-              type="text"
-              value={window.location.href}
-              readOnly
-              ref={inviteInput => {
-                this.inviteInput = inviteInput;
-              }}
-              onClick={e => e.target.select()}
-              className="form-field__input"
-            />
-            <Button
-              inverted
-              onClick={_ => {
-                this.inviteInput.select();
-                document.execCommand('copy');
-                showToast({
-                  text: `De link is gekopieerd naar jouw klembord, stuur het naar je vrienden!`
-                });
-              }}
-            >
-              Kopiëer
-            </Button>
-          </div>
-          <div className="card-block">
-            <label className="personName-label" htmlFor="personName">
-              Wie ben jij?
-            </label>
-            <input
-              className="form-field__input"
-              type="text"
-              name="personName"
-              id="personName"
-              onChange={changeUsernameCurrentUser}
-            />
-          </div>
-          <div
-            className="users"
-            style={{
-              position: 'absolute',
-              right: '0',
-              top: '0',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center'
-            }}
-          >
-            {Object.keys(room.users).length > 1 &&
-              <div>
-                <h4>Mensen in dit verhaal</h4>
-                {Object.keys(room.users).map(key => {
-                  return (
-                    <div key={key} style={{ verticalAlign: 'center' }}>
-                      <User />
-                      {room.users[key]}
-                    </div>
-                  );
-                })}
-              </div>}
-            <h4>Nodig iemand uit om mee te werken:</h4>
-            <input
-              type="text"
-              value={window.location.href}
-              readOnly
-              ref={inviteInput => {
-                this.inviteInput = inviteInput;
-              }}
-              onClick={e => e.target.select()}
-              className="form-field__input"
-            />
-            <Button
-              inverted
-              onClick={_ => {
-                this.inviteInput.select();
-                document.execCommand('copy');
-                showToast({
-                  text: `De link is gekopieerd naar jouw klembord, stuur het naar je vrienden!`
-                });
-              }}
-            >
-              Kopiëer
-            </Button>
-          </div>
-=======
             />
           </div>
 
@@ -250,7 +139,6 @@ class Room extends React.Component {
               }));
             }}
           />
->>>>>>> develop
 
           <div className="modules content-container">
             {room.modules &&

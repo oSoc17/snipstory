1e0d917f2c0715d730cf7f1aaed7ee9cd24f68e7
Elias Meire <eliasmeire@users.noreply.github.com>
2017-07-26 11:39:41 +0200

    Feature/work together ui (#84)
    * Footer fix links
    
    * work together styling + footer title
    
    * steps vertical
    
    * bigger floating next button
    
    * scroll top when location changes
    
    * fix imports

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,62 +4,50 @@ import logo1 from './assets/logo01.png';
 import logo2 from './assets/logo02.jpg';
 import logo3 from './assets/logo03.png';
 import logo4 from './assets/logo04.png';
-import logo5 from './assets/logo05.png';
-import logo6 from './assets/logo06.png';
-import logo7 from './assets/logo07.png';
-import logo8 from './assets/logo08.png';
+
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer>
       <div className="partner-container">
+        <h2 className="partner-title">In samenwerking met onze partners</h2>
         <div className="partner-mini-container row">
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-
-         
-
-            <a href="https://www.west-vlaanderen.be/">
+          <div className="logo col-xs-6 col-sm-4 col-md-3 col-lg-2">
+            <a
+              href="https://www.west-vlaanderen.be/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               <img className="effect img-fluid" src={logo1} alt="logo-1" />
             </a>
           </div>
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-            <a href="http://idrops.org/">
+          <div className="logo col-xs-6 col-sm-4 col-md-3 col-lg-2">
+            <a
+              href="http://idrops.org/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               <img className="effect img-fluid" src={logo2} alt="logo-2" />
             </a>
           </div>
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-            <a href="http://www.bienet.be/">
+          <div className="logo col-xs-6 col-sm-4 col-md-3 col-lg-2">
+            <a
+              href="http://www.bienet.be/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               <img className="effect img-fluid" src={logo3} alt="logo-3" />
             </a>
           </div>
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-            <a href="https://www.openknowledge.be/">
+          <div className="logo col-xs-6 col-sm-4 col-md-3 col-lg-2">
+            <a
+              href="https://www.openknowledge.be/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
               <img className="effect img-fluid" src={logo4} alt="logo-4" />
             </a>
-          </div>
-        </div>
-
-        <div className="partner-mini-container row">
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-            <a href="https://www.nsu.edu/">
-              <img className="effect img-fluid" src={logo5} alt="logo-5" />
-            </a>
-          </div>
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-            <a href="http://www.cwgc.org/find-war-dead.aspx">
-              <img className="effect img-fluid" src={logo6} alt="logo-6" />
-            </a>
-          </div>
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-            <a href="http://www.lijssenthoek.be">
-              <img className="effect img-fluid" src={logo7} alt="logo-7" />
-            </a>
-          </div>
-          <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-            <a href="http://www.inflandersfields.be/">
-              <img className="effect img-fluid" src={logo8} alt="logo-8" />
-            </a>
-
           </div>
         </div>
       </div>

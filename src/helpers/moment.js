19c662dbc7973fb10d389ae9ff2bd1d4c8940315
Elias Meire <eliasmeire@users.noreply.github.com>
2017-07-26 21:22:22 +0200

    Use nl-be locale and add meta tags (#92)
    * Styling fixes
    
    * date formatting
    
    * Use nl-be locale
    
    * more meta

--- src/helpers/moment.js
+++ src/helpers/moment.js
@@ -1,8 +1,8 @@
 import moment from 'moment';
-import 'moment/locale/nl';
+import 'moment/locale/nl-be';
 
 export const getReadableDate = (date = new Date()) => {
-  moment.locale('nl');
+  moment.locale('nl-be');
   return moment(date, 'DD/MM/YYYY').format('D MMMM YYYY');
 };
 

d9aaf07c591731dcb681493efa58566208aeba04
Bruce Boelens <bruceboelens@outlook.com>
2018-07-11 09:55:18 +0200

    Started working on fields of the general form
    Inserted 3 testfields to display on /teacher/addstories.

--- src/containers/views/AddStories/AddStories.js
+++ src/containers/views/AddStories/AddStories.js
@@ -1,40 +1,115 @@
 import React from 'react';
 import Button from '../../../components/button/Button';
+import Navbar from '../../../components/nav/Navbar';
+import Footer from '../../../components/footer/Footer';
+
+
 import { Link } from 'react-router-dom';
 import { Field, reduxForm, SubmissionError } from 'redux-form';
-import {
-  firebaseAuth,
-  googleAuthProvider,
-  firebaseDatabase
-} from '../../../helpers/firebase';
+import {firebaseAuth, googleAuthProvider, firebaseDatabase} from '../../../helpers/firebase';
 import FormField from '../../../components/form/FormField';
 import './AddStories.css'
 
-const AddStories = ({
-    fullName,
-    summary,
-    source,
-    // More to come
-    pristine,
-    submitting,
-    handleSubmit,
-    history,
-    error
-}) => {
+class AddStories extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            title: "",
+            summary: "",
+            source:"",
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState({
+            title: e.target.title.value,
+            summary: e.target.summary.value,
+            source: e.target.source.value,
+        });
+
+        console.log(this.state.summary);
+
+        // const storiesRef = firebase.firebaseDatabase().ref('stories');
+        // const story = {
+        //     title: this.state.title,
+        //     summary: this.state.summary,
+        //     source: this.state.source
+        // }
+        // storiesRef.push(story);
+        // this.setState({
+        //
+        // })
+
+
+    }
+
+
+    render() {
+        const {
+            user,
+            logout
+        } = this.props;
+
     return (
-        <div>
-            <h1 className="addStory-title">Add new stories</h1>
+        <div className="page">
+            <Navbar />
+            <h1>Voeg een verhaal toe</h1>
             <div>
-                <form
-                onSubmit={handleSubmit((stories) =>
-                    return firebaseDatabase
-                    .ref('/stories/${stories.uid}')
-                    .set({ ...stories.providerData[0], storyTitle: fullName})
-                    .then(_ => {
-                        hist
-                    })
-                )};
+                <p> Hi, {user.displayName}</p>
             </div>
+
+            <form onSubmit={this.handleSubmit}>
+            <div className="general-container">
+                <div>
+                    <Field
+                        name="title"
+                        component={FormField}
+                        type="text"
+                        label="Naam personage"
+                        required
+                    />
+                </div>
+                <div>
+                    <Field
+                        name="summary"
+                        component={FormField}
+                        type="text"
+                        label="Korte omschrijving"
+                        required
+                    />
+                </div>
+                <div>
+                    <Field
+                        name="source"
+                        component={FormField}
+                        type="text"
+                        label="Bron"
+                        required
+                    />
+                </div>
+                <div>
+                    <Field
+                        name="dayOfBirth"
+                        component={FormField}
+                        type="text"
+                        label="Bron"
+                        required
+                    />
+                </div>
+
+
+                <Button>Voeg een verhaal toe</Button>
+
+            <Button onClick={logout}>Uitloggen</Button>
+            </div>
+            </form>
+            <Footer />
         </div>
-    )
-}
+        );
+    };
+};
+
+export default reduxForm({ form: 'addStories'})(AddStories);
